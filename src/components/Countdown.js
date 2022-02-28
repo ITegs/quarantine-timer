f566b2bd01692eb0e7cd3444a4880bc0ef500fc8
Johannes <johannes-pahle@t-online.de>
2022-02-28 17:26:31 +0100

    remove createContext from `Countdown`

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Colors } from "../Theme/Colors";
 import { queryDate } from "./parameters";
 
